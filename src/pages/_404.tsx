1852c6a250ffe0cccaf04250d335a856cc73cb03
Joaquin Diaz <dev@joacod.com>
2025-06-15 16:55:46 -0300

    404 page

--- src/pages/_404.tsx
+++ src/pages/_404.tsx
@@ -1,8 +1,8 @@
 export function NotFound() {
-	return (
-		<section>
-			<h1>404: Not Found</h1>
-			<p>It's gone :(</p>
-		</section>
-	);
+  return (
+    <section className="flex flex-col items-center justify-center min-h-full p-4 text-center">
+      <h1 className="text-4xl font-bold mb-4">404: Not Found</h1>
+      <p className="text-lg text-gray-600">It's gone :(</p>
+    </section>
+  )
 }
